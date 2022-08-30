abf01131846e34881eab174bddb8dd31ec88b750
de Mesa, Adrian Kyle <adrian.kyle.de.mesa@oocl.com>
2022-08-30 17:46:01 +0800

    create added

---
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -0,0 +1,35 @@
+package com.tw;
+
+public class MultiplicationTable {
+    public String create(int start, int end) {
+
+        int result = start * end;
+        if (start != end) {
+            return null;
+        }else
+            return start + "*" + end + "=" + result;
+    }
+    public Boolean isValid(int start, int end) {
+        return null;
+    }
+    public Boolean isInRange(int number) {
+        return null;
+
+    }
+    public Boolean isStartNotBiggerThanEnd(int start, int end) {
+
+        return null;
+    }
+
+    public String generateTable(int start, int end) {
+        return null;
+    }
+
+    public String generateLine(int start, int row) {
+        return null;
+    }
+
+    public String generateSingleExpression(int multiplicand, int multiplier) {
+        return null;
+    }
+}
