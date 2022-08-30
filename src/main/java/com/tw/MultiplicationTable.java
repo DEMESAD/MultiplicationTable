011b53f480c860d60afe57525816aa91ef942f12
de Mesa, Adrian Kyle <adrian.kyle.de.mesa@oocl.com>
2022-08-30 17:51:11 +0800

    all added

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -41,10 +41,22 @@ public class MultiplicationTable {
     }
 
     public String generateLine(int start, int row) {
-        return null;
+        String lines = "";
+        for(int index = start; index <= row; index++){
+            String table = index +"*"+row+"="+index*row;
+            if(index != row){
+                lines += table+"  ";
+
+            } else {
+                lines += table;
+            }
+
+        }
+        return lines;
     }
 
     public String generateSingleExpression(int multiplicand, int multiplier) {
-        return null;
+        return multiplicand + "*" + multiplier + "=" + multiplicand*multiplier;
     }
 }
+
